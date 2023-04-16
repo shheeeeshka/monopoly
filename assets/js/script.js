6a1cc2f104a57e8a43d763128837e445aaa30c0c
shheeeeshka <libre.de.la.mort@yandex.ru>
2023-04-17 00:09:26 +0300

    flex adapted, chips added

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// function getRandom() {
+//     return Math.floor(Math.random() * (7 - 1) + 1);
+// }
+
+// let movesRandom = getRandom();
+
+// const chip = document.querySelector('.chip');
+
+// chip.style = 'margin-left: 12px';
