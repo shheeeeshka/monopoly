feb71e0988ad90181c4f9b59dc81f42792958d10
shheeeeshka <libre.de.la.mort@yandex.ru>
2023-04-22 00:14:14 +0300

    top section desc. added, main page created

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,36 @@
 "use strict";
 
+// let x = 0,
+//     y = 0;
+
+// document.addEventListener('keydown', function(e){
+//   if(e.keyCode == 37) y -= 5;
+//   if(e.keyCode == 39) y += 5;
+//   if(e.keyCode == 38) x += 5;
+//   if(e.keyCode == 40) x -= 5;
+ 
+//  document.querySelector('.cube').style.transform = `rotateY(${y}deg) rotateX(${x}deg)`;
+// });
+
 // function getRandom() {
 //     return Math.floor(Math.random() * (7 - 1) + 1);
 // }
 
-// let movesRandom = getRandom();
+// const uchip_1 = document.querySelector('.uchip-1'),
+//         section = document.querySelectorAll('.section'),
+//         movesRandom = getRandom();
+
+// if (movesRandom == 3) {
+//     uchip_1.style = 'margin-left: 190px';
+//     section.forEach(e => {
+//         e.style = 'background-color: orange';
+//     });
+// }
+
+// const section_description_1 = document.querySelector('.section-description-1'),
+//         section_description_brand_1 = section_description_1.querySelector('.brand'),
+//         section_description_2 = document.querySelector('.section-description-2'),
+//         section_description_brand_2 = section_description_2.querySelector('.brand');
+
+// section_description_1.style = 'display: flex';
+// section_description_2.style = 'display: flex';
